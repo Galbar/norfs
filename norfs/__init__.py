146a380b4d3cda4eb741c8d4dba351aa7e3a8a3e
Alessio Linares <alessio.linares@skyscanner.net>
2018-08-21 19:51:19 +0200

    Update version to 2.0.0

--- norfs/__init__.py
+++ norfs/__init__.py
@@ -1,7 +1,7 @@
 class Version:
-    MAJOR: str = "1"
-    MINOR: str = "3"
-    PATCH: str = "2"
+    MAJOR: str = "2"
+    MINOR: str = "0"
+    PATCH: str = "0"
 
     VERSION: str = ".".join((MAJOR, MINOR))
     RELEASE: str = ".".join((MAJOR, MINOR, PATCH))
